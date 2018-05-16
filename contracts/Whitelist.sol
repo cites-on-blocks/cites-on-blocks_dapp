ae059e8036a8c8fcf77cf53975fd1fc04eefad48
Alex <alexander.zoub@gmail.com>
2018-05-16 17:44:46 +0200

    (#7) Added comments and inline doc

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -8,9 +8,22 @@ contract Whitelist is Ownable {
 
   event AddressWhitelisted(address added, string indexed country);
 
+  /*
+  To be able to remove whole countries from the whitelist, we map a countrycode to 
+  all addresses related to that country.
+
+  All whitelisted addresses are assigned "true" in the whitelist mapping. If we want
+  to remove an address from the whitelist, we simply assign it "false" in the
+  witelist mapping.
+  
+  */
   mapping (string => address[]) authorityMapping;
   mapping (address => bool) whitelist; //TODO think about => type (as small as possible)
 
+
+  /**
+    Blocks all addresses mapped to false in the whitelist mapping.
+   */
   modifier onlyWhitelisted(){
     require(whitelist[msg.sender]);
     _; 
