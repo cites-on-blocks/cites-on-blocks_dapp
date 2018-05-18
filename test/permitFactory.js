6a3c0fe1c711da1aaddadb0fed444d536e9f1c34
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-05-18 18:54:36 +0200

    change test to use country codes

--- test/permitFactory.js
+++ test/permitFactory.js
@@ -16,21 +16,23 @@ contract('PermitFactory', accounts => {
 
   describe('#createPermit', () => {
     it('should create permit', done => {
-      const importer = '0x7af0244f9ea4612a6ffb371b0e7ad8973fb78ce4'
+      const exportCountry = 'DE'
+      const importCountry = 'FR'
       const permitType = 1
-      const imNameAndAddress = [asciiToHex('Importer Name'), asciiToHex('Importer Street'), asciiToHex('Importer City')]
-      const exNameAndAddress = [asciiToHex('Exporter Name'), asciiToHex('Exporter Street'), asciiToHex('Exporter City')]
+      const importer = ['Importer Name', 'Importer Street', 'Importer City']
+      const exporter = ['Exporter Name', 'Exporter Street', 'Exporter City']
       const quantities = [100]
-      const scientificNames = [asciiToHex('Scientific Name')]
-      const commonNames = [asciiToHex('Common Name')]
-      const descriptions = [asciiToHex('Description')]
-      const originHashes = [asciiToHex('')]
-      const reExportHashes = [asciiToHex('')]
+      const scientificNames = ['Scientific Name']
+      const commonNames = ['Common Name']
+      const descriptions = ['Description']
+      const originHashes = ['']
+      const reExportHashes = ['']
       permitFactoryInstance.createPermit(
-        importer,
+        exportCountry,
+        importCountry,
         permitType,
-        exNameAndAddress,
-        imNameAndAddress,
+        exporter,
+        importer,
         quantities,
         scientificNames,
         commonNames,
@@ -43,22 +45,23 @@ contract('PermitFactory', accounts => {
         const { permitHash } = log.args
         permitFactoryInstance.getPermit.call(permitHash, { from: accounts[0] }).then(permit => {
           const [
+            exportCountry,
+            importCountry,
+            permitType,
             exporter,
             importer,
-            permitType,
-            exNameAndAddress,
-            imNameAndAddress,
             specimenHashes,
             nonce
           ] = permit
-          assert.equal(exporter, accounts[0], 'Exporter is not msg.sender!')
-          assert.equal(importer, '0x7af0244f9ea4612a6ffb371b0e7ad8973fb78ce4', 'Wrong importer address!')
-          assert.equal(hexToUtf8(exNameAndAddress[0]), 'Exporter Name')
-          assert.equal(hexToUtf8(exNameAndAddress[1]), 'Exporter Street')
-          assert.equal(hexToUtf8(exNameAndAddress[2]), 'Exporter City')
-          assert.equal(hexToUtf8(imNameAndAddress[0]), 'Importer Name')
-          assert.equal(hexToUtf8(imNameAndAddress[1]), 'Importer Street')
-          assert.equal(hexToUtf8(imNameAndAddress[2]), 'Importer City')
+          assert.equal(hexToUtf8(exportCountry), 'DE')
+          assert.equal(hexToUtf8(importCountry), 'FR')
+          assert.equal(hexToUtf8(exporter[0]), 'Exporter Name')
+          assert.equal(hexToUtf8(exporter[1]), 'Exporter Street')
+          assert.equal(hexToUtf8(exporter[2]), 'Exporter City')
+          assert.equal(hexToUtf8(importer[0]), 'Importer Name')
+          assert.equal(hexToUtf8(importer[1]), 'Importer Street')
+          assert.equal(hexToUtf8(importer[2]), 'Importer City')
+          console.log(hexToUtf8(importer[0]))
           done()
         })
       }).catch(e => console.log('ERROR', e))
