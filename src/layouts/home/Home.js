7b4e009d0124c04425dd48d6a0439122189c9dbd
Patrick Reuter <patrickreuter@outlook.com>
2018-05-22 22:00:10 +0200

    Added map to the main page

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import worldmap from '../../imgs/world_map_cites.png'
+import { Box, Image } from 'grommet'
 
 class Home extends Component {
   render() {
     return (
       <main>
-        Hello World!
+        <Box pad={ { horizontal: 'large', vertical: 'large' } } >
+          <Image src={worldmap} fit="contain"/>
+        </Box>
       </main>
     )
   }
