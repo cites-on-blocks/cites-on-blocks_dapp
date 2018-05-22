0a1e1a2b0edbfd04bb25d0c2834a17087c131284
Alex <alexander.zoub@gmail.com>
2018-05-22 15:40:25 +0200

    Fixed a typo and corrected comment doc format

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -9,24 +9,24 @@ contract Whitelist is Ownable {
   event AddressWhitelisted(address added, string indexed country);
 
   /**
-    * Maps ISO 3166-1 Country Codes to a list of addresses assigned to that country
-  */
+   * Maps ISO 3166-1 Country Codes to a list of addresses assigned to that country
+   */
   mapping (bytes2 => address[]) authorityMapping; 
 
   /**
-    * Maps addresses to a boolean, indicating whether or not an address 
-    * is whitelisted
-  */
+   * Maps addresses to a boolean, indicating whether or not an address 
+   * is whitelisted
+   */
   mapping (address => bool) whitelist; 
 
   /**
-    * Maps addresses to the countries they are located in
-  */
+   * Maps addresses to the countries they are located in
+   */
   mapping (address => bytes2) authorityToCountry;
 
 
   /**
-    * Blocks all addresses mapped to false in the whitelist mapping.
+   * Blocks all addresses mapped to false in the whitelist mapping.
    */
   modifier onlyWhitelisted(){
     require(whitelist[msg.sender]);
@@ -34,9 +34,9 @@ contract Whitelist is Ownable {
   }
 
   /**
-    * Requires an address to belong to a specific country to proceed. 
-  */
-  modifier whitelistedForCountry(byte2 countryCode, address addr){
+   * Requires an address to belong to a specific country to proceed. 
+   */
+  modifier whitelistedForCountry(bytes2 countryCode, address addr) {
     require(authorityToCountry[addr]==countryCode);
     _;
   }
