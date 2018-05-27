cc6c31108b99814b628b7ed46dc75f37a1d34b9a
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-05-27 19:43:06 +0200

    add tests to createPermit and split tests

--- test/PermitFactory.js
+++ test/PermitFactory.js
@@ -8,10 +8,11 @@ contract('PermitFactory', accounts => {
   let whitelistInstance
 
   const ACC_OWNER = accounts[0]
-  const EXPORT_COUNTRY = 'DE'
   const ACC_EXPORT = accounts[1]
-  const IMPORT_COUNTRY = 'FR'
   const ACC_IMPORT = accounts[2]
+  const ACC_OTHER = accounts[3]
+  const EXPORT_COUNTRY = 'DE'
+  const IMPORT_COUNTRY = 'FR'
   const PERMIT_TYPE = 0
   const IMPORTER = ['Importer Name', 'Importer Street', 'Importer City']
   const EXPORTER = ['Exporter Name', 'Exporter Street', 'Exporter City']
@@ -23,7 +24,7 @@ contract('PermitFactory', accounts => {
   const RE_EXPORT_HASHES = ['', '']
 
   before(async () => {
-    // add addresses to whitelist so modifiers can be pleased
+    // add addresses to whitelist so modifiers can be satisfied
     permitFactoryInstance = await PermitFactory.deployed()
     await permitFactoryInstance.addAddress(ACC_EXPORT, EXPORT_COUNTRY, {
       from: ACC_OWNER
@@ -33,7 +34,6 @@ contract('PermitFactory', accounts => {
     })
   })
 
-  // TODO split up into separate tests
   describe('#createPermit', () => {
     it('should create permit', async () => {
       const result = await permitFactoryInstance.createPermit(
@@ -51,30 +51,55 @@ contract('PermitFactory', accounts => {
         { from: ACC_EXPORT }
       )
       const [ log ] = result.logs.filter(log => log.event === 'PermitCreated')
-      const { permitHash } = log.args
-      const permit = await permitFactoryInstance.getPermit.call(permitHash)
-      assert.equal(hexToUtf8(permit[0]), EXPORT_COUNTRY)
-      assert.equal(hexToUtf8(permit[1]), IMPORT_COUNTRY)
-      assert.equal(permit[2], PERMIT_TYPE)
-      assert.equal(hexToUtf8(permit[3][0]), EXPORTER[0])
-      assert.equal(hexToUtf8(permit[3][1]), EXPORTER[1])
-      assert.equal(hexToUtf8(permit[3][2]), EXPORTER[2])
-      assert.equal(hexToUtf8(permit[4][0]), IMPORTER[0])
-      assert.equal(hexToUtf8(permit[4][1]), IMPORTER[1])
-      assert.equal(hexToUtf8(permit[4][2]), IMPORTER[2])
-      assert.lengthOf(permit[5], QUANTITIES.length)
-      assert.isAbove(permit[6], 0)
-
-      // test specimen 1
-      const specimenHashes = permit[5]
-      const specimen_1 = await permitFactoryInstance.specimens(specimenHashes[0])
-      assert.equal(specimen_1[0], permitHash)
-      assert.equal(specimen_1[1].toString(), QUANTITIES[0].toString())
-      assert.equal(hexToUtf8(specimen_1[2]), SCIENTIFIC_NAMES[0])
-      assert.equal(hexToUtf8(specimen_1[3]), COMMON_NAMES[0])
-      assert.equal(hexToUtf8(specimen_1[4]), DESCRIPTIONS[0])
+      const { permitHash, exportCountry, importCountry } = log.args
+      assert.isString(permitHash)
+      assert.equal(hexToUtf8(exportCountry), EXPORT_COUNTRY)
+      assert.equal(hexToUtf8(importCountry), IMPORT_COUNTRY)
     })
-    // TODO more tests    
+
+    it('should not create permit for not whitelisted user', async () => {
+      try {
+        await permitFactoryInstance.createPermit(
+          EXPORT_COUNTRY,
+          IMPORT_COUNTRY,
+          PERMIT_TYPE,
+          EXPORTER,
+          IMPORTER,
+          QUANTITIES,
+          SCIENTIFIC_NAMES,
+          COMMON_NAMES,
+          DESCRIPTIONS,
+          ORIGIN_HASHES,
+          RE_EXPORT_HASHES,
+          { from: ACC_OTHER }
+        )
+        assert.fail()
+      } catch (err) {
+        // Expected to fail here.
+      }
+    })
+
+    it('should not create permit for wrong country', async () => {
+      try {
+        await permitFactoryInstance.createPermit(
+          'US',
+          IMPORT_COUNTRY,
+          PERMIT_TYPE,
+          EXPORTER,
+          IMPORTER,
+          QUANTITIES,
+          SCIENTIFIC_NAMES,
+          COMMON_NAMES,
+          DESCRIPTIONS,
+          ORIGIN_HASHES,
+          RE_EXPORT_HASHES,
+          { from: ACC_EXPORT }
+        )
+        assert.fail()
+      } catch (err) {
+        // Expected to fail here.
+      }
+    })
   })
 
   describe('#createPaperPermit', () => {
@@ -94,7 +119,81 @@ contract('PermitFactory', accounts => {
         { from: ACC_IMPORT }
       )
       const [ log ] = result.logs.filter(log => log.event === 'PermitCreated')
-      const { permitHash } = log.args
+      const { permitHash, exportCountry, importCountry } = log.args
+      assert.isString(permitHash)
+      assert.equal(hexToUtf8(exportCountry), EXPORT_COUNTRY)
+      assert.equal(hexToUtf8(importCountry), IMPORT_COUNTRY)
+    })
+
+    it('should not create paper permit for not whitelisted user', async () => {
+      try {
+        await permitFactoryInstance.createPermit(
+          EXPORT_COUNTRY,
+          IMPORT_COUNTRY,
+          PERMIT_TYPE,
+          EXPORTER,
+          IMPORTER,
+          QUANTITIES,
+          SCIENTIFIC_NAMES,
+          COMMON_NAMES,
+          DESCRIPTIONS,
+          ORIGIN_HASHES,
+          RE_EXPORT_HASHES,
+          { from: ACC_OTHER }
+        )
+        assert.fail()
+      } catch (err) {
+        // Expected to fail here.
+      }
+    })
+
+    it('should not create paper permit for wrong country', async () => {
+      try {
+        await permitFactoryInstance.createPermit(
+          EXPORT_COUNTRY,
+          'US',
+          PERMIT_TYPE,
+          EXPORTER,
+          IMPORTER,
+          QUANTITIES,
+          SCIENTIFIC_NAMES,
+          COMMON_NAMES,
+          DESCRIPTIONS,
+          ORIGIN_HASHES,
+          RE_EXPORT_HASHES,
+          { from: ACC_IMPORT }
+        )
+        assert.fail()
+      } catch (err) {
+        // Expected to fail here.
+      }
+    })
+  })
+
+  describe('#getPermit', () => {
+    let permitHash
+    let specimenHashes
+
+    before(async () => {
+      const result = await permitFactoryInstance.createPermit(
+        EXPORT_COUNTRY,
+        IMPORT_COUNTRY,
+        PERMIT_TYPE,
+        EXPORTER,
+        IMPORTER,
+        QUANTITIES,
+        SCIENTIFIC_NAMES,
+        COMMON_NAMES,
+        DESCRIPTIONS,
+        ORIGIN_HASHES,
+        RE_EXPORT_HASHES,
+        { from: ACC_EXPORT }
+      )
+      const [ log ] = result.logs.filter(log => log.event === 'PermitCreated')
+      permitHash = log.args.permitHash
+    })
+
+    it('should return permit', async () => {
       const permit = await permitFactoryInstance.getPermit.call(permitHash)
       assert.equal(hexToUtf8(permit[0]), EXPORT_COUNTRY)
       assert.equal(hexToUtf8(permit[1]), IMPORT_COUNTRY)
@@ -107,8 +206,17 @@ contract('PermitFactory', accounts => {
       assert.equal(hexToUtf8(permit[4][2]), IMPORTER[2])
       assert.lengthOf(permit[5], QUANTITIES.length)
       assert.isAbove(permit[6], 0)
+      specimenHashes = permit[5]      
     })
-    // TODO more tests    
+
+    it('should return specimen', async () => {
+      const specimen_1 = await permitFactoryInstance.specimens(specimenHashes[0])
+      assert.equal(specimen_1[0], permitHash)
+      assert.equal(specimen_1[1].toString(), QUANTITIES[0].toString())
+      assert.equal(hexToUtf8(specimen_1[2]), SCIENTIFIC_NAMES[0])
+      assert.equal(hexToUtf8(specimen_1[3]), COMMON_NAMES[0])
+      assert.equal(hexToUtf8(specimen_1[4]), DESCRIPTIONS[0])
+    })
   })
 
   describe('#confirmPermit', () => {
