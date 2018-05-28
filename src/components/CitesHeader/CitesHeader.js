780555cd4e265d84a694837e8032a275f19a79f8
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-05-28 18:21:21 +0200

    add different permit pages for create, process and list

--- src/components/CitesHeader/CitesHeader.js
+++ src/components/CitesHeader/CitesHeader.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
-import { Header, Image, Menu, Title } from 'grommet'
+import { Header, Image, Menu, Title, Anchor } from 'grommet'
 import logo from '../../imgs/croco.png'
 import s from '../../localization/localizedStrings'
 
@@ -16,13 +16,21 @@ class CitesHeader extends Component {
         align={'end'}
         justify="between"
         separator="bottom"
-        pad={{ horizontal: 'small', vertical: 'small' }}>
+        pad={{ horizontal: 'small' }}>
         <Title>
           <Image src={logo} alt="logo" size="thumb" />
         </Title>
         <Menu direction={'row'}>
           <Link to="/whitelist">{s.whitelist}</Link>
-          <Link to="/permits">{s.permits}</Link>
+          <Menu
+            responsive={true}
+            label={s.permits}
+            inline={false}
+            direction={'column'}>
+            <Anchor path="/permits">List Permits</Anchor>
+            <Anchor path="/permits/create">Create Permit</Anchor>
+            <Anchor path="/permits/process">Process Permit</Anchor>
+          </Menu>
           <Link to="/analytics">{s.analytics}</Link>
           <Link to="/import-export">{s.importExport}</Link>
           <Link to="/help">{s.help}</Link>
