c3248b90e8eaa1428e6632a7c8cf6a3dd976de8d
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-06-23 15:55:01 +0200

    add production config

--- truffle.js
+++ truffle.js
@@ -1,10 +1,18 @@
 module.exports = {
-  migrations_directory: "./migrations",
+  migrations_directory: './migrations',
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: '*' // Match any network id
+    },
+    production: {
+      // TODO: replace with ip and port of azure instance
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      // NOTE: unlocked account with a lot of ETH -> controller
+      from: '0x6b0c56d1ad5144b4d37fa6e27dc9afd5c2435c3b'
     }
   },
   solc: {
@@ -12,5 +20,5 @@ module.exports = {
       enabled: true,
       runs: 500
     }
-  } 
-};
+  }
+}
