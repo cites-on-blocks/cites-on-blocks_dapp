281f7c5d6eee7c8d62cc400598ae10b4e3c06d9b
dakingha69 <dongha.kim210@gmail.com>
2018-08-14 14:40:18 +0200

    change production host to correct ip

--- truffle.js
+++ truffle.js
@@ -7,11 +7,12 @@ module.exports = {
       network_id: '*' // Match any network id
     },
     production: {
-      host: 'localhost',
+      host: '40.115.39.123',
       port: 8545,
       network_id: '*',
       // NOTE: unlocked account with a lot of ETH -> controller
-      from: '0x6b0c56d1ad5144b4d37fa6e27dc9afd5c2435c3b'
+      from: '0x6b0c56d1ad5144b4d37fa6e27dc9afd5c2435c3b',
+      gas: '0x2FEFD8'
     }
   },
   solc: {
